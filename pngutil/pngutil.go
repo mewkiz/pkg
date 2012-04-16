718925f8ca6c045d209e874e6174f28a277d1bfd
mewmew <random0x00@gmail.com>
2012-04-16 23:10:41 +0200

    pngutil: Added ReadFile function.

--- pngutil/pngutil.go
+++ pngutil/pngutil.go
@@ -5,7 +5,22 @@ import "image"
 import "image/png"
 import "os"
 
-// WriteFile writes the image data to a file specified by imgPath. WriteFile
+// ReadFile reads a PNG file specified by imgPath and returns it as an
+// image.Image.
+func ReadFile(imgPath string) (img image.Image, err error) {
+   fr, err := os.Open(imgPath)
+   if err != nil {
+      return nil, err
+   }
+   defer fr.Close()
+   img, err = png.Decode(fr)
+   if err != nil {
+      return nil, err
+   }
+   return img, nil
+}
+
+// WriteFile writes the image data to a PNG file specified by imgPath. WriteFile
 // creates the named file using mode 0666 (before umask), truncating it if it
 // already exists.
 func WriteFile(imgPath string, img image.Image) (err error) {
