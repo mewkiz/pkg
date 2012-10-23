38ea55ead84fe3b17fac9c83d19a72515b2a5e9b
mewmew <random0x00@gmail.com>
2012-10-23 18:50:36 +0200

    proxy: Added package proxy. all: Added documentation to readme.

--- README.md
+++ README.md
@@ -4,6 +4,31 @@ pkg
 This project provides packages for various utility functions and commonly used
 features.
 
+documentation
+-------------
+
+Documentation provided by GoPkgDoc:
+
+   - [bufioutil][]
+   - [bytesutil][]
+   - [errorsutil][]
+   - [htmlutil][]
+   - [httputil][]
+   - [imgutil][]
+   - [proxy][]
+   - [stringsutil][]
+   - [term][]
+
+[bufioutil]: http://go.pkgdoc.org/github.com/mewkiz/pkg/bufioutil
+[bytesutil]: http://go.pkgdoc.org/github.com/mewkiz/pkg/bytesutil
+[errorsutil]: http://go.pkgdoc.org/github.com/mewkiz/pkg/errorsutil
+[htmlutil]: http://go.pkgdoc.org/github.com/mewkiz/pkg/htmlutil
+[httputil]: http://go.pkgdoc.org/github.com/mewkiz/pkg/httputil
+[imgutil]: http://go.pkgdoc.org/github.com/mewkiz/pkg/imgutil
+[proxy]: http://go.pkgdoc.org/github.com/mewkiz/pkg/proxy
+[stringsutil]: http://go.pkgdoc.org/github.com/mewkiz/pkg/stringsutil
+[term]: http://go.pkgdoc.org/github.com/mewkiz/pkg/term
+
 public domain
 -------------
 
---
+++ proxy/proxy.go
@@ -0,0 +1,27 @@
+// Package proxy provides proxy server utility functions.
+package proxy
+
+import "fmt"
+import "net/http"
+import "net/http/httputil"
+
+// Listen initiates a reverse proxy on a given port. It handles incoming client
+// requests and sends them to their intended target server, proxying the
+// response back to the client.
+//
+// director has the ability to modify requests before they are sent. It can also
+// be used to simply perform an action based on the requests.
+func Listen(port int, director func(req *http.Request)) (err error) {
+   // handle handles incoming client requests and sends them to their intended
+   // target server, proxying the response back to the client.
+   handle := func(w http.ResponseWriter, req *http.Request) {
+      proxy := httputil.ReverseProxy{Director: director}
+      proxy.ServeHTTP(w, req)
+   }
+   http.HandleFunc("/", handle)
+   err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+   if err != nil {
+      return err
+   }
+   return nil
+}
