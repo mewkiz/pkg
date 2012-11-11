caefa7e7f588f94b805361fc6bd79f0991398f9a
mewmew <random0x00@gmail.com>
2012-11-11 02:39:12 +0100

    readerutil: Use io.ReadFull instead of r.Read.

--- readerutil/readerutil.go
+++ readerutil/readerutil.go
@@ -6,7 +6,7 @@ import "io"
 // ReadByte reads and returns one byte from the provided io.Reader.
 func ReadByte(r io.Reader) (b byte, err error) {
 	buf := make([]byte, 1)
-	_, err = r.Read(buf)
+	_, err = io.ReadFull(r, buf)
 	if err != nil {
 		return 0, err
 	}
