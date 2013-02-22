d403b3c8870b0433f202f387167cb4e0ef666f8f
mewmew <rnd0x00@gmail.com>
2013-02-22 17:42:12 +0100

    gobase: fix SrcDir after last commit.

--- goutil/gotuil.go
+++ goutil/gotuil.go
@@ -8,7 +8,7 @@ import "path/filepath"
 // if successful. GOPATH may contain a list of multiple paths.
 func SrcDir(dir string) (srcDir string, ok bool) {
 	for _, goPath := range filepath.SplitList(os.Getenv("GOPATH")) {
-		srcDir = goPath + "src/" + dir
+		srcDir = goPath + "/src/" + dir
 		_, err := os.Stat(srcDir)
 		if err == nil {
 			return srcDir, true
