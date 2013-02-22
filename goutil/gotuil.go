765d9c9ea29c1e7cbb75be06d0b7b77b9ed12ff0
mewmew <rnd0x00@gmail.com>
2013-02-22 17:32:22 +0100

    goutil: make dir in SrcDir relative to GOPATH/src/
    ref: https://github.com/mewkiz/pkg/issues/3

--- goutil/gotuil.go
+++ goutil/gotuil.go
@@ -4,11 +4,11 @@ package goutil
 import "os"
 import "path/filepath"
 
-// SrcDir tries to locate dir in GOPATH and returns it's full path and true if
-// successful. GOPATH may contain a list of multiple paths.
+// SrcDir tries to locate dir in GOPATH/src/ and returns it's full path and true
+// if successful. GOPATH may contain a list of multiple paths.
 func SrcDir(dir string) (srcDir string, ok bool) {
 	for _, goPath := range filepath.SplitList(os.Getenv("GOPATH")) {
-		srcDir = goPath + dir
+		srcDir = goPath + "src/" + dir
 		_, err := os.Stat(srcDir)
 		if err == nil {
 			return srcDir, true
