c1a0e2003e33a77a70071c9036ccae51ac52586b
mewmew <rnd0x00@gmail.com>
2013-02-24 01:01:56 +0100

    goutil: SrcDir now returns (string, error) instead of (string, bool).

--- goutil/gotuil.go
+++ goutil/gotuil.go
@@ -1,18 +1,19 @@
 // Package goutil implements some golang relevant utility functions.
 package goutil
 
+import "fmt"
 import "os"
 import "path/filepath"
 
 // SrcDir tries to locate dir in GOPATH/src/ and returns it's full path and true
 // if successful. GOPATH may contain a list of multiple paths.
-func SrcDir(dir string) (srcDir string, ok bool) {
+func SrcDir(dir string) (srcDir string, err error) {
 	for _, goPath := range filepath.SplitList(os.Getenv("GOPATH")) {
-		srcDir = goPath + "/src/" + dir
+		srcDir = fmt.Sprintf("%s/src/%s", goPath, dir)
 		_, err := os.Stat(srcDir)
 		if err == nil {
-			return srcDir, true
+			return srcDir, nil
 		}
 	}
-	return "", false
+	return "", fmt.Errorf("goutil.SrcDir: unable to locate directory (%q) in GOPATH/src/ (%q).", dir, os.Getenv("GOPATH"))
 }
