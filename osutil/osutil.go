0720af08189dc31c0c84e45d45dfe7134fdab35b
Henry Eklind <henry@vaert.se>
2013-03-10 20:34:36 +0100

    Added osutil.Exists

---
+++ osutil/osutil.go
@@ -0,0 +1,14 @@
+// Package httputil implements some os utility functions.
+package osutil
+
+// Exists returns whether the given file or directory exists or not.
+func Exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
