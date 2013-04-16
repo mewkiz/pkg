4d2f3710fccc3b1c05c8bac228b05670f7c0adbb
mewmew <rnd0x00@gmail.com>
2013-04-16 15:16:50 +0200

    pathutil: Add Base type which helps prevent directory traversal vulnerabilities.

--- pathutil/pathutil.go
+++ pathutil/pathutil.go
@@ -2,11 +2,31 @@
 package pathutil
 
 import (
+	"fmt"
 	"path"
+	"strings"
 )
 
 // TrimExt returns a slice of the string filePath without the extension.
 func TrimExt(filePath string) string {
 	ext := path.Ext(filePath)
 	return filePath[:len(filePath)-len(ext)]
+}
+
+// Base corresponds to a base directory.
+type Base string
+
+// Path returns a sanitized concatenation of base and relPath. The
+// implementation takes extra precausions to avoid directory traversal
+// vulnerabilities.
+func (base Base) Path(relPath string) (string, error) {
+	// Join joins the path elements and cleans the result p.
+	p := path.Join(string(base), relPath)
+	// If relPath contains directory traversal characters such as "../" p
+	// could have escaped base by now.
+	if !strings.HasPrefix(p, string(base)) {
+		// Prevent directory traversal.
+		return "", fmt.Errorf("Base.Path: path %q doesn't contain the prefix %q.", p, base)
+	}
+	return p, nil
 }
