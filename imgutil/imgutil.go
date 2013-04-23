cfc8821482a85f41ed4b789886f49ac6355c8cbe
mewmew <rnd0x00@gmail.com>
2013-04-23 04:21:52 +0200

    imgutil: Add Equal and ColorEq functions to locate equivalent images.

--- imgutil/imgutil.go
+++ imgutil/imgutil.go
@@ -1,11 +1,14 @@
 // Package imgutil implements some image utility functions.
 package imgutil
 
-import "image"
-import _ "image/gif"
-import _ "image/jpeg"
-import "image/png"
-import "os"
+import (
+	"image"
+	"image/color"
+	_ "image/gif"
+	_ "image/jpeg"
+	"image/png"
+	"os"
+)
 
 // ReadFile reads an image file (gif, jpeg or png) specified by imgPath and
 // returns it as an image.Image.
@@ -36,4 +39,57 @@ func WriteFile(imgPath string, img image.Image) (err error) {
 		return err
 	}
 	return nil
+}
+
+// Equal returns true if the images img1 and img2 are equal, and false
+// otherwise.
+func Equal(img1, img2 image.Image) bool {
+	rect1 := img1.Bounds()
+	rect2 := img2.Bounds()
+
+	// Compare bounds.
+	if rect1 != rect2 {
+		return false
+	}
+
+	// Compare pixel colors.
+	for x := rect1.Min.X; x < rect1.Max.X; x++ {
+		for y := rect1.Min.Y; y < rect1.Max.Y; y++ {
+			c1 := img1.At(x, y)
+			c2 := img2.At(x, y)
+			if !ColorEq(c1, c2) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+// ColorEq returns true if the colors c1 and c2 are equal, and false otherwise.
+func ColorEq(c1, c2 color.Color) bool {
+	r1, g1, b1, a1 := c1.RGBA()
+	r2, g2, b2, a2 := c2.RGBA()
+
+	// Compare red.
+	if r1 != r2 {
+		return false
+	}
+
+	// Compare green.
+	if g1 != g2 {
+		return false
+	}
+
+	// Compare blue.
+	if b1 != b2 {
+		return false
+	}
+
+	// Compare alpha.
+	if a1 != a2 {
+		return false
+	}
+
+	return true
 }
