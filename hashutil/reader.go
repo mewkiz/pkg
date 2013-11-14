1d1a10151c72b6bd9c0b2ef7ee419800b7444143
mewmew <rnd0x00@gmail.com>
2013-11-14 16:34:11 +0100

    hashutil: Make it possible to disable hashing for Hash16Reader.

--- hashutil/reader.go
+++ hashutil/reader.go
@@ -5,7 +5,8 @@ import (
 )
 
 // Hash16Reader is the interface that wraps the Hash16 and io.Reader interfaces.
-// All data read from the io.Reader is also added to the running hash.
+// All data read from the io.Reader is also added to the running hash, unless
+// Hash16 is set to nil.
 type Hash16Reader interface {
 	Hash16
 	io.Reader
@@ -18,7 +19,7 @@ type hash16Reader struct {
 }
 
 // NewHash16Reader returns a Hash16Reader based on the provided io.Reader. All
-// data read is also added to the running hash.
+// data read is also added to the running hash, unless Hash16 is set to nil.
 func NewHash16Reader(r io.Reader, h Hash16) (hr Hash16Reader) {
 	hr = &hash16Reader{
 		Hash16: h,
@@ -28,9 +29,12 @@ func NewHash16Reader(r io.Reader, h Hash16) (hr Hash16Reader) {
 }
 
 // Read reads up to len(buf) bytes into buf. All data read is also added to the
-// running hash. It returns the number of bytes read (0 <= n <= len(buf)) and
-// any error encountered.
+// running hash, unless Hash16 is set to nil. It returns the number of bytes
+// read (0 <= n <= len(buf)) and any error encountered.
 func (hr *hash16Reader) Read(buf []byte) (n int, err error) {
+	if hr.Hash16 == nil {
+		return hr.Reader.Read(buf)
+	}
 	n, err = hr.Reader.Read(buf)
 	if n > 0 {
 		_, e := hr.Hash16.Write(buf[:n])
