2b8a92813d0d827d6fb44cff2bd31bf46eb7e21d
mewmew <rnd0x00@gmail.com>
2013-11-15 16:09:15 +0100

    hashutil: Refactor hash reader and add test case for CRC8 hash reader.

--- hashutil/reader.go
+++ hashutil/reader.go
@@ -1,46 +1,53 @@
 package hashutil
 
 import (
+	"hash"
 	"io"
 )
 
-// Hash16Reader is the interface that wraps the Hash16 and io.Reader interfaces.
-// All data read from the io.Reader is also added to the running hash, unless
-// Hash16 is set to nil.
-type Hash16Reader interface {
-	Hash16
+// hashReader wraps the basic hash.Hash and io.Reader interfaces. All data read
+// from the io.Reader is also added to the running hash, unless Hash is set to
+// nil.
+type hashReader struct {
+	hash.Hash
 	io.Reader
 }
 
-// hash16Reader is a simple implementation of the Hash16Reader interface.
-type hash16Reader struct {
-	Hash16
-	io.Reader
-}
-
-// NewHash16Reader returns a Hash16Reader based on the provided io.Reader. All
-// data read is also added to the running hash, unless Hash16 is set to nil.
-func NewHash16Reader(r io.Reader, h Hash16) (hr Hash16Reader) {
-	hr = &hash16Reader{
-		Hash16: h,
+// NewHashReader returns a HashReader based on the provided io.Reader. All data
+// read is also added to the running hash, unless Hash is set to nil.
+func NewHashReader(r io.Reader, h hash.Hash) (hr *hashReader) {
+	hr = &hashReader{
+		Hash:   h,
 		Reader: r,
 	}
 	return hr
 }
 
 // Read reads up to len(buf) bytes into buf. All data read is also added to the
-// running hash, unless Hash16 is set to nil. It returns the number of bytes
-// read (0 <= n <= len(buf)) and any error encountered.
-func (hr *hash16Reader) Read(buf []byte) (n int, err error) {
-	if hr.Hash16 == nil {
+// running hash, unless Hash is set to nil. It returns the number of bytes read
+// (0 <= n <= len(buf)) and any error encountered.
+func (hr *hashReader) Read(buf []byte) (n int, err error) {
+	if hr.Hash == nil {
 		return hr.Reader.Read(buf)
 	}
 	n, err = hr.Reader.Read(buf)
 	if n > 0 {
-		_, e := hr.Hash16.Write(buf[:n])
+		_, e := hr.Hash.Write(buf[:n])
 		if err == nil && e != nil {
 			return n, e
 		}
 	}
 	return n, err
+}
+
+// Sum8 returns the 8-bit checksum of the hash. It panics if hr.Hash doesn't
+// implement the interface Hash8.
+func (hr *hashReader) Sum8() uint8 {
+	return hr.Hash.(Hash8).Sum8()
+}
+
+// Sum16 returns the 16-bit checksum of the hash. It panics if hr.Hash doesn't
+// implement the interface Hash16.
+func (hr *hashReader) Sum16() uint16 {
+	return hr.Hash.(Hash16).Sum16()
 }
