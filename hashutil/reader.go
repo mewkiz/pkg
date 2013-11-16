a54da466514ba1113d96be288af91b8959621722
mewmew <rnd0x00@gmail.com>
2013-11-16 16:17:15 +0100

    hashutil: Export HashReader.

--- hashutil/reader.go
+++ hashutil/reader.go
@@ -5,18 +5,18 @@ import (
 	"io"
 )
 
-// hashReader wraps the basic hash.Hash and io.Reader interfaces. All data read
+// HashReader wraps the basic hash.Hash and io.Reader interfaces. All data read
 // from the io.Reader is also added to the running hash, unless Hash is set to
 // nil.
-type hashReader struct {
+type HashReader struct {
 	hash.Hash
 	io.Reader
 }
 
 // NewHashReader returns a HashReader based on the provided io.Reader. All data
 // read is also added to the running hash, unless Hash is set to nil.
-func NewHashReader(r io.Reader, h hash.Hash) (hr *hashReader) {
-	hr = &hashReader{
+func NewHashReader(r io.Reader, h hash.Hash) (hr *HashReader) {
+	hr = &HashReader{
 		Hash:   h,
 		Reader: r,
 	}
@@ -26,7 +26,7 @@ func NewHashReader(r io.Reader, h hash.Hash) (hr *hashReader) {
 // Read reads up to len(buf) bytes into buf. All data read is also added to the
 // running hash, unless Hash is set to nil. It returns the number of bytes read
 // (0 <= n <= len(buf)) and any error encountered.
-func (hr *hashReader) Read(buf []byte) (n int, err error) {
+func (hr *HashReader) Read(buf []byte) (n int, err error) {
 	if hr.Hash == nil {
 		return hr.Reader.Read(buf)
 	}
@@ -42,24 +42,24 @@ func (hr *hashReader) Read(buf []byte) (n int, err error) {
 
 // Sum8 returns the 8-bit checksum of the hash. It panics if hr.Hash doesn't
 // implement the interface Hash8.
-func (hr *hashReader) Sum8() uint8 {
+func (hr *HashReader) Sum8() uint8 {
 	return hr.Hash.(Hash8).Sum8()
 }
 
 // Sum16 returns the 16-bit checksum of the hash. It panics if hr.Hash doesn't
 // implement the interface Hash16.
-func (hr *hashReader) Sum16() uint16 {
+func (hr *HashReader) Sum16() uint16 {
 	return hr.Hash.(Hash16).Sum16()
 }
 
 // Sum32 returns the 32-bit checksum of the hash. It panics if hr.Hash doesn't
 // implement the interface hash.Hash32.
-func (hr *hashReader) Sum32() uint32 {
+func (hr *HashReader) Sum32() uint32 {
 	return hr.Hash.(hash.Hash32).Sum32()
 }
 
 // Sum64 returns the 64-bit checksum of the hash. It panics if hr.Hash doesn't
 // implement the interface hash.Hash64.
-func (hr *hashReader) Sum64() uint64 {
+func (hr *HashReader) Sum64() uint64 {
 	return hr.Hash.(hash.Hash64).Sum64()
 }
