88a41cfd40a5e0a155b7ad8730bb4d697d699c2a
mewmew <rnd0x00@gmail.com>
2013-12-15 23:43:19 +0100

    goutil: Add AbsImpPath function.

--- goutil/gotuil.go
+++ goutil/gotuil.go
@@ -3,19 +3,41 @@ package goutil
 
 import (
 	"fmt"
+	"go/build"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
-// SrcDir tries to locate dir in GOPATH/src/ and returns it's full path and true
-// if successful. GOPATH may contain a list of multiple paths.
-func SrcDir(dir string) (srcDir string, err error) {
-	for _, goPath := range filepath.SplitList(os.Getenv("GOPATH")) {
-		srcDir = fmt.Sprintf("%s/src/%s", goPath, dir)
-		_, err := os.Stat(srcDir)
+// SrcDir tries to locate dir in GOPATH/src/ or GOROOT/src/pkg/ and returns its
+// full path and true if successful. GOPATH may contain a list of multiple
+// paths.
+func SrcDir(dir string) (absDir string, err error) {
+	for _, srcDir := range build.Default.SrcDirs() {
+		absDir = filepath.Join(srcDir, dir)
+		_, err := os.Stat(absDir)
 		if err == nil {
-			return srcDir, nil
+			return absDir, nil
 		}
 	}
-	return "", fmt.Errorf("goutil.SrcDir: unable to locate directory (%q) in GOPATH/src/ (%q).", dir, os.Getenv("GOPATH"))
+	return "", fmt.Errorf("goutil.SrcDir: unable to locate directory (%q) in GOPATH/src/ (%q) or GOROOT/src/pkg/ (%q).", dir, os.Getenv("GOPATH"), os.Getenv("GOROOT"))
+}
+
+// AbsImpPath tries to locate the absolute import path based on the provided
+// relative import path.
+func AbsImpPath(relImpPath string) (absImpPath string, err error) {
+	absPath, err := filepath.Abs(relImpPath)
+	if err != nil {
+		return "", fmt.Errorf("goutil.AbsImpPath: %v", err)
+	}
+	for _, srcDir := range build.Default.SrcDirs() {
+		if strings.HasPrefix(absPath, srcDir) {
+			absImpPath := absPath[len(srcDir):]
+			if len(absImpPath) > 0 && absImpPath[0] == '/' {
+				absImpPath = absImpPath[1:]
+			}
+			return absImpPath, nil
+		}
+	}
+	return "", fmt.Errorf("goutil.AbsImpPath: unable to locate absolute import path for %q", relImpPath)
 }
