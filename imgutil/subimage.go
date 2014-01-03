490375f9ec40c8f96ebf03818e3d704c73926e44
mewmew <rnd0x00@gmail.com>
2014-01-03 21:50:39 +0100

    imgutil: Add SubImager and SubFallback.

---
+++ imgutil/subimage.go
@@ -0,0 +1,37 @@
+package imgutil
+
+import (
+	"image"
+	"image/draw"
+)
+
+// SubImager is an interface that extends the basic image.Image interface with
+// the SubImage method.
+type SubImager interface {
+	image.Image
+	SubImage(r image.Rectangle) image.Image
+}
+
+// SubFallback returns the provided image.Image as a SubImager. It provides a
+// fallback for images missing the SubImage method.
+func SubFallback(img image.Image) SubImager {
+	sub, ok := img.(SubImager)
+	if !ok {
+		return &subFallback{Image: img}
+	}
+	return sub
+}
+
+// subFallback provides a fallback SubImage method for images.
+type subFallback struct {
+	image.Image
+}
+
+// SubImage returns an image representing the portion of the image src visible
+// through r. The returned value doesn't shares pixels with the original image.
+func (src *subFallback) SubImage(r image.Rectangle) image.Image {
+	dstRect := image.Rect(0, 0, r.Dx(), r.Dy())
+	dst := image.NewRGBA(dstRect)
+	draw.Draw(dst, dstRect, src, r.Min, draw.Over)
+	return dst
+}
