74df591dbc7e10acff9f7675a4ac74a3b0701c16
mewmew <rnd0x00@gmail.com>
2014-08-07 00:41:18 +0200

    bits: Rename package bitutil to package bits and simplify function names.

--- bits/unary.go
+++ bits/unary.go
@@ -1,12 +1,12 @@
-// Package bitutil implements common binary encoding and decoding algorithms.
-package bitutil
+// Package bits implements common binary encoding and decoding algorithms.
+package bits
 
 import (
 	"github.com/mewkiz/pkg/bit"
 )
 
-// DecodeUnary decodes and returns an unary coded integer, whose value is
-// represented by the number of leading zeros before a one.
+// Unary decodes and returns an unary coded integer, whose value is represented
+// by the number of leading zeros before a one.
 //
 // Examples of unary coded binary on the left and decoded decimal on the right:
 //
@@ -17,7 +17,7 @@ import (
 //    00001   => 4
 //    000001  => 5
 //    0000001 => 6
-func DecodeUnary(br *bit.Reader) (n uint64, err error) {
+func Unary(br *bit.Reader) (n uint64, err error) {
 	for {
 		bit, err := br.Read(1)
 		if err != nil {
--- bits/zigzag.go
+++ bits/zigzag.go
@@ -1,6 +1,6 @@
-package bitutil
+package bits
 
-// DecodeZigZag decodes a ZigZag encoded integer and returns it.
+// ZigZag decodes a ZigZag encoded integer and returns it.
 //
 // Examples of ZigZag encoded values on the left and decoded values on the
 // right:
@@ -14,6 +14,6 @@ package bitutil
 //     3 => 6
 //
 // ref: https://developers.google.com/protocol-buffers/docs/encoding
-func DecodeZigZag(x int32) int32 {
+func ZigZag(x int32) int32 {
 	return x>>1 ^ -(x & 1)
 }
