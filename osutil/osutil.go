72fa4f94e1e49b3dbeb9afc54ec5cd07dc40df97
Robin Eklind <mewmew@users.noreply.github.com>
2016-03-07 01:32:23 +0100

    Update osutil.go
    Minor doc comment fix.

--- osutil/osutil.go
+++ osutil/osutil.go
@@ -5,7 +5,7 @@ import (
 	"os"
 )
 
-// Exists returns whether the given file or directory exists or not.
+// Exists reports whether the given file or directory exists or not.
 func Exists(path string) (bool, error) {
 	_, err := os.Stat(path)
 	if err == nil {
