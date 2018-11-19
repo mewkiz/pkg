9729f4f4ff2bb0cfa96b0929d335196be35fecdc
Robin Eklind <rnd0x00@gmail.com>
2018-11-19 13:25:51 +0100

    jsonutil: annotate error of jsonutil.Parse with pkg/errors

--- jsonutil/jsonutil.go
+++ jsonutil/jsonutil.go
@@ -14,7 +14,10 @@ import (
 func Parse(r io.Reader, v interface{}) error {
 	br := bufio.NewReader(r)
 	dec := json.NewDecoder(br)
-	return dec.Decode(v)
+	if err := dec.Decode(v); err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
 }
 
 // ParseFile parses the given JSON file into v.
@@ -24,7 +27,5 @@ func ParseFile(path string, v interface{}) error {
 		return errors.WithStack(err)
 	}
 	defer f.Close()
-	br := bufio.NewReader(f)
-	dec := json.NewDecoder(br)
-	return dec.Decode(v)
+	return Parse(f, v)
 }
