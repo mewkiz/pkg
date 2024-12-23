7f3c7df797fff0dc33be077f5b7ad69e22db6ca0
Robin Eklind <rnd0x00@gmail.com>
2024-12-23 23:07:03 +0100

    iterutil: simplify Ordered by using slices.Sorted

--- iterutil/iterutil.go
+++ iterutil/iterutil.go
@@ -11,8 +11,7 @@ import (
 // Ordered returns an iterator over key-value pairs from m.
 // The iteration order is deterministic, arranging keys in ascending order.
 func Ordered[Map ~map[K]V, K cmp.Ordered, V any](m Map) iter.Seq2[K, V] {
-	keys := slices.Collect(maps.Keys(m))
-	slices.Sort(keys)
+	keys := slices.Sorted(maps.Keys(m))
 	return func(yield func(K, V) bool) {
 		for _, k := range keys {
 			v := m[k]
